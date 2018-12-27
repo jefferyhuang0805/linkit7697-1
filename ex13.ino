dcfcb0a2b0abfa54a5b2b6876bed50e03497f623
Lidarsheu <lidarsheu@gmail.com>
2018-12-27 15:17:25 +0800

    Update ex13.ino

--- ex13.ino
+++ ex13.ino
@@ -9,12 +9,14 @@ LiquidCrystal_I2C lcd_i2c(0x27);
 int t; //溫度
 int h; //濕度
 int s; //土壤濕度
-int th = 27; //風扇自動開關溫度預設值
+int f; //風扇
+int w; //噴水
+int th = 25; //風扇自動開關溫度預設值
 char ss[3]; //土壤濕度文字格式
 char _lwifi_ssid[] = "lita";
 char _lwifi_pass[] = "0911027768";
 
-MCSDevice mcs("DF07gb0C", "V9q47IqgA0LYq19q");
+MCSDevice mcs("DXw243yd", "CfxiZLEs0UXcSie4");
 MCSDisplayInteger temp("temp"); //溫度
 MCSDisplayInteger humid("humid"); //濕度
 MCSDisplayInteger soil("soil"); //土壤濕度
@@ -31,9 +33,13 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT); //風扇(內建LED)
   pinMode(2, OUTPUT); //噴水(外接LED)
 
+  //新增通道id
   mcs.addChannel(temp);
   mcs.addChannel(humid);
-  mcs.addChannel(MySwitch); //通道id
+  mcs.addChannel(MySwitch); 
+  mcs.addChannel(MySwitch1);
+  mcs.addChannel(slider);
+  
   Serial.println("Wi-Fi 開始連線");
   while (WiFi.begin(_lwifi_ssid, _lwifi_pass) != WL_CONNECTED) { delay(1000); }
   Serial.println("Wi-Fi 連線成功");
@@ -44,8 +50,8 @@ void setup()
 void loop()
 {
   while (!mcs.connected()) {
-  	mcs.connect();
-  	if (mcs.connected()) { Serial.println("MCS 已重新連線"); }
+    mcs.connect();
+    if (mcs.connected()) { Serial.println("MCS 已重新連線"); }
   }
   mcs.process(100);
 
@@ -56,11 +62,6 @@ void loop()
   s = 100 - map(s, 281,4095, 0, 100); //轉換土壤濕度為 0-100
   sprintf(ss, "%3d", s); //轉換土壤濕度為文字格式
   
-  // MCS儀錶板顯示
-  temp.set(t); //溫度
-  humid.set(h); //濕度
-  soil.set(s); //土壤濕度
-
   // Serial port視窗顯示
   Serial.print("溫度=");
   Serial.println(t);
@@ -68,36 +69,35 @@ void loop()
   Serial.println(h);
   Serial.print("土壤濕度=");
   Serial.println(s);
+
+  // MCS儀錶板顯示
+  temp.set(t);
+  humid.set(h);
+  soil.set(s);
   
   // 更新開啟風扇溫度
   if (slider.updated()) {
     th = slider.value();
+    Serial.print("風扇開啟溫度=");
+    Serial.println(th);
   }
+  //檢查開關
+  if (MySwitch.updated()) {
+    if (MySwitch.value()) {f = 1;
+        } else {f = 0;}
+  }      
+  if (MySwitch1.updated()) {
+    if (MySwitch1.value()) {w = 1;
+        } else {w = 0;}
+ }      
 
   // 風扇控制
-  if (MySwitch.updated() || t > th){
-    Serial.print("開啟風扇降溫");
-    Serial.println(MySwitch.value());
-
-    if (MySwitch.value() || t > th) {
-      digitalWrite(LED_BUILTIN, HIGH); //風扇(內建LED ON)
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-  }
-
+  if (t >= th || f == 1 ) digitalWrite(LED_BUILTIN, HIGH);
+  if (t < th && f == 0 ) digitalWrite(LED_BUILTIN, LOW);
   // 噴水控制
-  if (MySwitch1.updated() || s < 50){
-    Serial.print("開啟噴水");
-    Serial.println(MySwitch1.value());
-
-    if (MySwitch1.value() || s < 50) {
-      digitalWrite(2, HIGH); //噴水(外接LED ON)
-    } else {
-      digitalWrite(2, LOW);
-    }
-  }
-  
+  if (s <= 50 || w == 1 ) {digitalWrite(2, HIGH);}
+  if (s > 50 &&  w== 0 ) {digitalWrite(2, LOW);}
+      
   // LCD顯示
   lcd_i2c.setCursor(0,0);
   lcd_i2c.print("T=");
@@ -120,4 +120,3 @@ void loop()
   
   delay(1000);
 }
-
